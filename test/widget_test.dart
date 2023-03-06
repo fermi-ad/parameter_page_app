59024d5a0626323d47d45d77979550f5f6c7756d
Rich Neswold <rich.neswold@gmail.com>
2023-03-06 16:11:19 -0600

    :bug: fix test so it compiles

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,10 @@ import 'package:parameter_page/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      const FermiApp(
+          title: "Parameter Page", child: BaseWidget(title: 'Parameter Page')),
+    );
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
