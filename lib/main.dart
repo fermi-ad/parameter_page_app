2e60a1a981b4fe898767cbcd7b6fe98c97a6d62c
Rich Neswold <rich.neswold@gmail.com>
2023-03-06 15:55:18 -0600

    :recycle: rename `MyApp` to `FermiApp`

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FermiApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FermiApp extends StatelessWidget {
+  const FermiApp({super.key});
 
   // This widget is the root of your application.
   @override
