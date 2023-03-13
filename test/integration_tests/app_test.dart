fb20f7552d1f707d06508c1beeb60ac9cdd56b14
John Diamond <jdiamond@stalag13.net>
2023-03-13 10:10:55 -0500

    GREEN - test that G:AMANDA is on the page

--- test/integration_tests/app_test.dart
+++ test/integration_tests/app_test.dart
@@ -3,11 +3,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:parameter_page/main.dart' as app;
+import 'package:parameter_page/page/page.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('Parameter Page End-to-End Tests', () {
+  group('Smoke Tests', () {
     testWidgets('Enter the parameter page, title is set to Parameter Page',
         (tester) async {
       // Given the app is running
@@ -16,6 +17,21 @@ void main() {
 
       // Then 'Parameter Page' is displayed in the title bar
       _assertAppBarTitleIs('Parameter Page');
+    });
+  });
+
+  group('Display Parameter Page', () {
+    testWidgets('Display test page, should contain three parameters',
+        (tester) async {
+      // Given the test page is loaded
+      app.main();
+      await tester.pumpAndSettle();
+
+      // Then 'M:OUTTMP' should be displayed
+      final page = find.byType(PageWidget);
+      final parameterFinder = find.text("G:AMANDA");
+      expect(
+          find.descendant(of: page, matching: parameterFinder), findsOneWidget);
     });
   });
 }
