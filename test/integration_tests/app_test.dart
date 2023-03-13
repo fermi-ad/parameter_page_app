54193cf93e60d1be7ffc3a4ce0002139482a177f
John Diamond <jdiamond@stalag13.net>
2023-03-13 12:03:01 -0500

    Remove unused import

--- test/integration_tests/app_test.dart
+++ test/integration_tests/app_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:parameter_page/main.dart' as app;
-import 'package:parameter_page/page/page.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
