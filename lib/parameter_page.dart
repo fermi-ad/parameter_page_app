1779d65eb2ce0164b27dae70ea05cd759fbcc992
John Diamond <jdiamond@stalag13.net>
2023-04-17 14:59:49 -0500

    GREEN - passing unit test for creating a ParameterPage with an empty list

---
+++ lib/parameter_page.dart
@@ -0,0 +1,9 @@
+import 'package:parameter_page/page/entry.dart';
+
+class ParameterPage {
+  List<PageEntry> entriesAsList() {
+    return _entries;
+  }
+
+  final List<PageEntry> _entries = [];
+}
--- test/unit_tests/parameter_page_test.dart
+++ test/unit_tests/parameter_page_test.dart
@@ -1,9 +1,14 @@
+import 'package:parameter_page/parameter_page.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('ParameterPage unit tests', () {
-    test("example test, runs", () {
-      expect(1, 1);
+    test("New ParameterPage, entry list is empty", () {
+      // When I instantiate a new ParameterPage
+      ParameterPage page = ParameterPage();
+
+      // Then the entries list is empty
+      expect(page.entriesAsList().length, 0);
     });
   });
 }
