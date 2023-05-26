4a2a991f34382521acb747b214a49aa9c5b9d859
John Diamond <jdiamond@stalag13.net>
2023-05-26 10:14:17 -0500

    GREEN - integration test file for Display Parameter Alarm Min/Max/Tolerance  acceptance tests

---
+++ test/integration_tests/display_parameter_alarms_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:parameter_page/main.dart' as app;
+
+import 'helpers.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('Change Parameter Alarm Min/Max/Tolerance', () {
+    testWidgets('Placeholder', (tester) async {
+      // Given
+      // When
+      // Then
+    });
+  });
+}
