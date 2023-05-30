34c016192b670e77842e4f8ddf8ebf9be5b5bc8c
John Diamond <jdiamond@stalag13.net>
2023-05-30 09:56:39 -0500

    GREEN - created a widget test file for ParameterWidget

---
+++ test/widget_tests/parameter_widget_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:parameter_page/widgets/display_settings_widget.dart';
+import 'package:parameter_page/widgets/parameter_widget.dart';
+
+void main() {
+  group("ParameterWidget", () {
+    assertAlarmDetails({required bool areVisible}) {}
+
+    testWidgets('showAlarmDetails false, alarm details are not displayed',
+        (WidgetTester tester) async {
+      // Given nothing...
+      // When I instantiate and display a ParameterEntry with showAlarmDetails = false
+      const parameterWidgetApp = MaterialApp(
+          home: Scaffold(body: ParameterWidget("M:OUTTMP", false, true)));
+      await tester.pumpAndSettle();
+
+      // Then the alarm details are not displayed
+      assertAlarmDetails(areVisible: false);
+    });
+  });
+}
