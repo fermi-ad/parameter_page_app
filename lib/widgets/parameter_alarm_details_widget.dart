28c9ca90b563fa678b86cc6806e1a23a5f5ae0ce
John Diamond <jdiamond@stalag13.net>
2023-06-02 11:38:56 -0500

    REFACTOR - moved the alarm details to be positioned below the reading/setting values

--- lib/widgets/parameter_alarm_details_widget.dart
+++ lib/widgets/parameter_alarm_details_widget.dart
@@ -13,40 +13,38 @@ class ParameterAlarmDetailsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     const textStyle = TextStyle(color: Colors.grey, fontSize: 12.0);
     const valueStyle = TextStyle(fontSize: 12.0);
-    return SizedBox(
-        width: 250,
-        child: Column(children: [
-          Row(key: Key("parameter_alarm_nominal_$drf"), children: [
-            const Text(
-                overflow: TextOverflow.ellipsis, "Nominal: ", style: textStyle),
-            Text(
-                overflow: TextOverflow.ellipsis,
-                alarmBlock.nominal,
-                style: valueStyle),
-            Row(key: Key("parameter_alarm_tolerance_$drf"), children: [
-              const Text(" (+/- ", style: textStyle),
-              Text(alarmBlock.tolerance, style: valueStyle),
-              const Text(")", style: textStyle)
-            ])
-          ]),
-          Row(children: [
-            Row(key: Key("parameter_alarm_min_$drf"), children: [
-              const Text(
-                  overflow: TextOverflow.ellipsis, "Min: ", style: textStyle),
-              Text(
-                  overflow: TextOverflow.ellipsis,
-                  alarmBlock.min,
-                  style: valueStyle)
-            ]),
-            Row(key: Key("parameter_alarm_max_$drf"), children: [
-              const Text(
-                  overflow: TextOverflow.ellipsis, " Max: ", style: textStyle),
-              Text(
-                  overflow: TextOverflow.ellipsis,
-                  alarmBlock.max,
-                  style: valueStyle)
-            ])
-          ]),
-        ]));
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Row(key: Key("parameter_alarm_nominal_$drf"), children: [
+        const Text(
+            overflow: TextOverflow.ellipsis, "Nominal: ", style: textStyle),
+        Text(
+            overflow: TextOverflow.ellipsis,
+            alarmBlock.nominal,
+            style: valueStyle),
+        Row(key: Key("parameter_alarm_tolerance_$drf"), children: [
+          const Text(" (+/- ", style: textStyle),
+          Text(alarmBlock.tolerance, style: valueStyle),
+          const Text(")", style: textStyle)
+        ])
+      ]),
+      Row(children: [
+        Row(key: Key("parameter_alarm_min_$drf"), children: [
+          const Text(
+              overflow: TextOverflow.ellipsis, "Min: ", style: textStyle),
+          Text(
+              overflow: TextOverflow.ellipsis,
+              alarmBlock.min,
+              style: valueStyle)
+        ]),
+        Row(key: Key("parameter_alarm_max_$drf"), children: [
+          const Text(
+              overflow: TextOverflow.ellipsis, " Max: ", style: textStyle),
+          Text(
+              overflow: TextOverflow.ellipsis,
+              alarmBlock.max,
+              style: valueStyle)
+        ])
+      ]),
+    ]);
   }
 }
