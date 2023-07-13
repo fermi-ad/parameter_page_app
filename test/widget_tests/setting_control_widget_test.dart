663b044455773a0975e81406f6316f665959c743
John Diamond <jdiamond@stalag13.net>
2023-07-13 10:47:20 -0500

    REFACTOR - extracted duplicate code into initialize(..)

--- test/widget_tests/setting_control_widget_test.dart
+++ test/widget_tests/setting_control_widget_test.dart
@@ -3,15 +3,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:parameter_page/widgets/setting_control_widget.dart';
 
 void main() {
+  MaterialApp initialize(Widget child) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Row(
+                key: const Key("parameter_setting_Z:BTE200_TEMP"),
+                children: [child])));
+  }
+
   group("SettingControlWidget", () {
     testWidgets('Provide no initial value, displays 0.0',
         (WidgetTester tester) async {
       // Given a SettingControlWidget instantiated for a device called Z:BTE200_TEMP
-      MaterialApp app = const MaterialApp(
-          home: Scaffold(
-              body: Row(
-                  key: Key("parameter_setting_Z:BTE200_TEMP"),
-                  children: [SettingControlWidget(drf: "Z:BTE200_TEMP")])));
+      MaterialApp app =
+          initialize(const SettingControlWidget(drf: "Z:BTE200_TEMP"));
 
       // When I display the setting
       await tester.pumpWidget(app);
@@ -27,11 +32,8 @@ void main() {
     testWidgets('Provide an initial value, displays that value',
         (WidgetTester tester) async {
       // Given a SettingControlWidget instantiated for a device called Z:BTE200_TEMP
-      MaterialApp app = const MaterialApp(
-          home: Scaffold(
-              body: Row(key: Key("parameter_setting_Z:BTE200_TEMP"), children: [
-        SettingControlWidget(drf: "Z:BTE200_TEMP", value: "72.0")
-      ])));
+      MaterialApp app = initialize(
+          const SettingControlWidget(drf: "Z:BTE200_TEMP", value: "72.0"));
 
       // When I display the setting
       await tester.pumpWidget(app);
