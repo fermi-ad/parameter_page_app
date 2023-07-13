02ad284fc03a3a3ab1c34ef2341e5f83c50742ee
John Diamond <jdiamond@stalag13.net>
2023-07-13 10:43:57 -0500

    GREEN - passing test for displaying a provided value

--- test/widget_tests/setting_control_widget_test.dart
+++ test/widget_tests/setting_control_widget_test.dart
@@ -23,5 +23,25 @@ void main() {
               matching: find.text("0.0")),
           findsOneWidget);
     });
+
+    testWidgets('Provide an initial value, displays that value',
+        (WidgetTester tester) async {
+      // Given a SettingControlWidget instantiated for a device called Z:BTE200_TEMP
+      MaterialApp app = const MaterialApp(
+          home: Scaffold(
+              body: Row(key: Key("parameter_setting_Z:BTE200_TEMP"), children: [
+        SettingControlWidget(drf: "Z:BTE200_TEMP", value: "72.0")
+      ])));
+
+      // When I display the setting
+      await tester.pumpWidget(app);
+
+      // Then 0.0 is displayed
+      expect(
+          find.descendant(
+              of: find.byKey(const Key("parameter_setting_Z:BTE200_TEMP")),
+              matching: find.text("72.0")),
+          findsOneWidget);
+    });
   });
 }
