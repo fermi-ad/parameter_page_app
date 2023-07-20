49d6e0bbd9c49266af0b1649430890d2690179f3
John Diamond <jdiamond@stalag13.net>
2023-07-20 14:49:44 -0500

    Corrected the expected number of rows

--- test/integration_tests/add_parameter_to_page_test.dart
+++ test/integration_tests/add_parameter_to_page_test.dart
@@ -23,7 +23,7 @@ void main() {
       await waitForDataToLoadFor(tester, "Z:BDCCT");
 
       // Then the new parameter is added to the page
-      assertParameterIsInRow("Z:BDCCT", 7);
+      assertParameterIsInRow("Z:BDCCT", 8);
       assertParameterHasDetails("Z:BDCCT",
           description: "device description",
           settingValue: "50.00",
