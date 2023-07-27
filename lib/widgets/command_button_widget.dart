287193b54c7aff6ef8bfb72d5958c3752f551000
John Diamond <jdiamond@stalag13.net>
2023-07-27 13:01:02 -0500

    REFACTOR - replaced a bunch of conditionals with a switch that dispatches to various _build methods depending on _displayState

--- lib/widgets/command_button_widget.dart
+++ lib/widgets/command_button_widget.dart
@@ -27,28 +27,53 @@ enum _CommandButtonDisplayState { ready, pending, error }
 class _CommandButtonState extends State<CommandButtonWidget> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: Stack(alignment: Alignment.center, children: [
-          if (_displayState == _CommandButtonDisplayState.pending)
-            const Icon(Icons.pending),
-          ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor:
-                      _displayState == _CommandButtonDisplayState.error
-                          ? Colors.red
-                          : Colors.blue,
-                  elevation: 0.0,
-                  minimumSize: const Size.fromHeight(40)),
-              onPressed: _displayState == _CommandButtonDisplayState.pending
-                  ? null
-                  : _handlePress,
-              child: Text(
-                  style: const TextStyle(color: Colors.white),
-                  _displayState == _CommandButtonDisplayState.error
-                      ? _errorMessage!
-                      : widget.longName))
-        ]));
+    return Padding(padding: const EdgeInsets.all(4.0), child: _buildButton());
+  }
+
+  Widget _buildButton() {
+    switch (_displayState) {
+      case _CommandButtonDisplayState.ready:
+        return _buildReadyButton();
+      case _CommandButtonDisplayState.pending:
+        return _buildPendingButton();
+      case _CommandButtonDisplayState.error:
+        return _buildErrorButton();
+    }
+  }
+
+  Widget _buildReadyButton() {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            elevation: 0.0,
+            minimumSize: const Size.fromHeight(40)),
+        onPressed: _handlePress,
+        child: Text(widget.longName));
+  }
+
+  Widget _buildPendingButton() {
+    return Stack(alignment: Alignment.center, children: [
+      const Icon(Icons.pending),
+      ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              elevation: 0.0,
+              minimumSize: const Size.fromHeight(40)),
+          onPressed: null,
+          child: Text(
+              style: const TextStyle(color: Colors.white), widget.longName))
+    ]);
+  }
+
+  Widget _buildErrorButton() {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            elevation: 0.0,
+            minimumSize: const Size.fromHeight(40)),
+        onPressed: _handlePress,
+        child:
+            Text(style: const TextStyle(color: Colors.white), _errorMessage!));
   }
 
   void _handlePress() {
