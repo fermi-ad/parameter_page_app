26dcbb0cf57b090e74c14c9e717e560526821709
John Diamond <jdiamond@stalag13.net>
2023-07-28 12:55:29 -0500

    Putting the open_page_appbar key back in

--- lib/widgets/open_page_widget.dart
+++ lib/widgets/open_page_widget.dart
@@ -15,8 +15,8 @@ class OpenPageWidget extends StatefulWidget {
   @override
   State<OpenPageWidget> createState() => _OpenPageWidgetState();
 }
- 
-class _OpenPageWidgetState extends State<OpenPageWidget>{
+
+class _OpenPageWidgetState extends State<OpenPageWidget> {
   List<dynamic> titles = [];
   final Logger logger = Logger();
 
@@ -25,79 +25,89 @@ class _OpenPageWidgetState extends State<OpenPageWidget>{
     super.initState();
     _fetchData(); // Fetch initial data when the widget is initialized
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(40),
         child: AppBar(
+          key: const Key("open_page_appbar"),
           backgroundColor: Colors.blue,
           foregroundColor: Colors.white,
           title: Text(
-              widget.title, 
-              style: const TextStyle(fontSize: 17, fontWeight: FontWeight.bold, fontStyle:FontStyle.normal),
-              ),  
+            widget.title,
+            style: const TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.normal),
+          ),
         ),
       ),
-      body: 
-          Column(
-            children: [
-              Expanded(
-                child: TitleQueryWrapper(
-                          titles: titles,
-                          fetchData: _fetchData,
-                        ),
-              ),
-              Container(
-                      padding:const EdgeInsets.all(10), 
-                      child: 
-                         Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            GestureDetector (
-                              onTap: ()=> {const CircularProgressIndicator(backgroundColor: Colors.amber,),
-                              _fetchData()},
-                              child: const Text('<Refresh>', style:TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.blue)),
-                            ),
-                            GestureDetector (
-                              onTap: () async {
-                                await showDialog(
-                                  context: context, 
-                                  builder: (BuildContext dialogContext){
-                                    return NewTitleDialog(titles:titles, fetchData:_fetchData);
-                                  },
-                                );
-                              },
-                              child: const Text('<Add page title>', style:TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.blue)),
-                            ),
-                          ],
-                        )
-                      ),
-            ],
+      body: Column(
+        children: [
+          Expanded(
+            child: TitleQueryWrapper(
+              titles: titles,
+              fetchData: _fetchData,
+            ),
           ),
+          Container(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: () => {
+                      const CircularProgressIndicator(
+                        backgroundColor: Colors.amber,
+                      ),
+                      _fetchData()
+                    },
+                    child: const Text('<Refresh>',
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue)),
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      await showDialog(
+                        context: context,
+                        builder: (BuildContext dialogContext) {
+                          return NewTitleDialog(
+                              titles: titles, fetchData: _fetchData);
+                        },
+                      );
+                    },
+                    child: const Text('<Add page title>',
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue)),
+                  ),
+                ],
+              )),
+        ],
+      ),
     );
   }
 
-
-  Future <void> _fetchData() async {
+  Future<void> _fetchData() async {
     final QueryOptions options = QueryOptions(
-            document: gql(titlequery),
-            fetchPolicy: FetchPolicy.noCache,
-        );
+      document: gql(titlequery),
+      fetchPolicy: FetchPolicy.noCache,
+    );
 
     final QueryResult result = await client.value.query(options);
 
     if (result.hasException) {
-          logger.e('GraphQL Error: ${result.exception}');
-          } 
-    else {
-        setState(() {
-          titles = [];
-          titles = result.data? ['allTitles'];
-        });  
+      logger.e('GraphQL Error: ${result.exception}');
+    } else {
+      setState(() {
+        titles = [];
+        titles = result.data?['allTitles'];
+      });
     }
   } //fetchData function
-
-
 }
