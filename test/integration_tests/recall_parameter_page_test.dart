51bf7cb382bd7b3aa0b0de82d0019827392f57fe
John Diamond <jdiamond@stalag13.net>
2023-08-11 15:23:21 -0500

    REFACTOR - Added a test page called Test Page 1 to the MockParameterPageService that has all of the hard-coded entries we've been using as our test page up until now

--- test/integration_tests/recall_parameter_page_test.dart
+++ test/integration_tests/recall_parameter_page_test.dart
@@ -77,9 +77,41 @@ void main() {
       assertPageTitleIs("Test Page 1");
 
       // ... and the contents of Test Page 1 are loaded
+      assertIsOnPage(comment: "This is our first comment!");
+      assertParametersAreOnPage([
+        "M:OUTTMP@e,02",
+        "G:AMANDA",
+        "Z:NO_ALARMS",
+        "PIP2:SSR1:SUBSYSTEMA:SUBSUBSYSTEM:TEMPERATURE",
+        "PIP2:SSR1:SUBSYSTEMA:SUBSUBSYSTEM:HUMIDITY",
+        "Z:BTE200_TEMP",
+        "Z:INC_SETTING"
+      ]);
+    }, semanticsEnabled: false);
+
+    testWidgets('Select Test Page 2, return to main page and load Test Page 2',
+        (WidgetTester tester) async {
+      // Given I am on the "Open Parameter Page" page
+      app.main();
+      await waitForMainPageToLoad(tester);
+      await navigateToOpenPage(tester);
+
+      // When I select Test Page 1
+      await openParameterPage(tester, withTitle: 'Test Page 2');
+
+      // Then I should be returned to the main page
+      assertOpenPage(isVisible: false);
+
+      // ... and the page loading progress indicator should be gone
+      assertOpeningPageProgressIndicator(isVisible: false);
+
+      // ... and the title should be displayed
+      assertPageTitleIs("Test Page 2");
+
+      // ... and the contents of Test Page 1 are loaded
       assertIsOnPage(comment: "this is comment #1");
       assertParametersAreOnPage(["I:BEAM", "R:BEAM"]);
       assertIsOnPage(comment: "this is comment #2");
-    });
+    }, semanticsEnabled: false);
   });
 }
