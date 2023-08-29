1de1439d906432470900fe35da0c03eff758e52f
John Diamond <jdiamond@stalag13.net>
2023-08-29 07:02:58 -0500

    RED - failing acceptance test for disabled copy link menu item

---
+++ test/integration_tests/generate_link_to_parameter_page_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'helpers/assertions.dart';
+import 'helpers/actions.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('Generate Link to Parameter Page', () {
+    testWidgets(
+        'For new/un-saved parameter page, copy link menu item is disabled',
+        (WidgetTester tester) async {
+      // Given I am working on a brand-new, never-persisted parameter page
+      await startParameterPageApp(tester);
+      await createNewParameterPage(tester);
+
+      // When I open the main menu
+      await openMainMenu(tester);
+
+      // Then the "Copy Link" menu item is disabled
+      assertMainMenuItem(tester, name: "Copy Link", isEnabled: false);
+    }, semanticsEnabled: false);
+  });
+}
