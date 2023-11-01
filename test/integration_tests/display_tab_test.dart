a429927f72662e939ef69c8543d504e4fa2e82e5
John Diamond <jdiamond@stalag13.net>
2023-11-01 09:46:38 -0500

    Obsolete the test for displaying a tab on Test Page 1

--- test/integration_tests/display_tab_test.dart
+++ test/integration_tests/display_tab_test.dart
@@ -8,17 +8,6 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('Display Parameter Page', () {
-    testWidgets('Open test page 1, one tab should be visible', (tester) async {
-      // Given the parameter page is started
-      await startParameterPageApp(tester);
-
-      // When I open Test Page 1
-      await navigateToTestPage1(tester);
-
-      // Then two tabs are visible:
-      assertTabBarContains(nTabs: 1, withTitles: ["Tab 1"]);
-    });
-
     testWidgets('Open test page 2, two tabs should be visible', (tester) async {
       // Given the parameter page is started
       await startParameterPageApp(tester);
