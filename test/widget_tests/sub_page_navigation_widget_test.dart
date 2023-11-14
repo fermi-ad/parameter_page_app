22b4143bf278d02e39cace495ee1dc58c0045ba5
John Diamond <jdiamond@stalag13.net>
2023-11-14 06:48:13 -0600

    GREEN - Adding widget test script for SubPageNavigationWidget

---
+++ test/widget_tests/sub_page_navigation_widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("SubPageNavigationWidget", () {
+    testWidgets('Current sub-page indicator, shows the current sub-page',
+        (WidgetTester tester) async {});
+  });
+}
