2b1c996ae520b529723e2e6151a30ae102044fd1
John Diamond <jdiamond@stalag13.net>
2023-11-14 14:47:44 -0600

    RED - failing widget test for displaying the total number of sub-pages

--- test/widget_tests/sub_page_navigation_widget_test.dart
+++ test/widget_tests/sub_page_navigation_widget_test.dart
@@ -21,15 +21,40 @@ void main() {
       await tester.pumpWidget(app);
 
       // Then the current sub-page is 2
-      _assertCurrentSubPage(isSetTo: 2);
+      _assertCurrentSubPageIs(2);
+    });
+
+    testWidgets(
+        'Sub-page count indicator, displays the number of sub-pages for the current tab',
+        (WidgetTester tester) async {
+      // Given a ParameterPage with 3 sub-pages
+      ParameterPage page = ParameterPage();
+      page.enableEditing();
+      page.createSubPage();
+      page.createSubPage();
+
+      // When I render the SubPageNavigationWidget
+      MaterialApp app = MaterialApp(home: SubPageNavigationWidget(page: page));
+      await tester.pumpWidget(app);
+
+      // Then the number of sub-pages is 3
+      _assertNumberOfSubPagesIs(3);
     });
   });
 }
 
-void _assertCurrentSubPage({required int isSetTo}) {
+void _assertCurrentSubPageIs(int isSetTo) {
   expect(
       find.descendant(
           of: find.byKey(const Key("subpagenavigation-current-subpage")),
           matching: find.text("$isSetTo")),
+      findsOneWidget);
+}
+
+void _assertNumberOfSubPagesIs(int numberOfSubPagesIs) {
+  expect(
+      find.descendant(
+          of: find.byKey(const Key("subpagenavigation-total-subpages")),
+          matching: find.text("$numberOfSubPagesIs")),
       findsOneWidget);
 }
