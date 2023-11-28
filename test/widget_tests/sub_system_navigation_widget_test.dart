00974144b43aa763a24f2f6af747aa498c0217ee
John Diamond <jdiamond@fnal.gov>
2023-11-28 10:11:16 -0600

    Adding sub_system_navigation_widget_test

---
+++ test/widget_tests/sub_system_navigation_widget_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:parameter_page/entities/parameter_page.dart';
+import 'package:parameter_page/widgets/sub_page_navigation_widget.dart';
+
+import '../integration_tests/helpers/actions.dart';
+import '../integration_tests/helpers/assertions.dart';
+
+void main() {
+  group("SubSystemNavigationWidget", () {
+    testWidgets('Current sub-system indicator, shows the current sub-system',
+        (WidgetTester tester) async {});
+  });
+}
