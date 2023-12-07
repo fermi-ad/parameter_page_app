a8b67f75b1648cc609bc566b060c587680e82fba
jimh <115260636+huaji168@users.noreply.github.com>
2023-12-07 10:49:33 -0600

    API to return tree

--- lib/services/parameter_page/gql_param/mutations.dart
+++ lib/services/parameter_page/gql_param/mutations.dart
@@ -1,88 +1,105 @@
 
-const addpagetitle = r"""
-        mutation addTitle($title: String!) {
-          addTitle(title: $title) {
-            pageid
-            title
+/******************************************************************** 
+ name: add_defaultpagetree
+ input: parameter page title (string)
+ output: one complete tree structures of single default branche and values
+         of one parameter page, including tree title, one sub-system, 
+         one tab, one sub-page, no user entries.
+**********************************************************************/
+const add_defaultpagetree = r"""
+             mutation newDefaultPageTree($title: String!) {
+                newParamPage(title: $title) {
+                  pageid
+                  title
+                  sub_systems:subsyslist {
+                    subsysid
+                    title
+                    seqnum
+                    tabs:subsystablist {
+                      subsystabid
+                      title
+                      seqnum
+                      sub_pages:tabpagelist {
+                        tabpageid
+                        title
+                        seqnum
+                      }
+                    }
+                  }
+                }
+              }
+          """;
+
+
+
+/******************************************************************** 
+ name: merge_entries
+      (use to merge(insert, update or mix of both) those new/updated entries,
+      IF transaction deletion is included, run delete_entries first)
+ input: list of entries(one or more) to merge, format of 
+        [
+            {
+              tabpageid, -- same subpage id for all entries in one list
+              position, -- ending position before submit
+              text_old, -- old value at current position, null for new entry
+              text_new, -- new value for current position
+              type_old, -- old value at current position, null for new entry
+              type_new  -- new value for current position
+            },
+            {...}
+          ]
+        order the list by position at descending order.
+        e.g.
+                "mrgEntries": [
+            {
+              "tabpageid": 3,
+              "position": 31,
+              "text_old": null,
+              "text_new": "high temperature",
+              "type_old": null,
+              "type_new": "Comments"
+            },
+          {
+              "tabpageid": 3,
+              "position": 21,
+              "text_old": "magnetics",
+              "text_new": "electrical magnetics",
+              "type_old": "Comments",
+              "type_new": "Comments"
+            }
+          ]
+ output: transaction return code and message. 
+          (return code, 1: succeed, -1: failed)
+**********************************************************************/ 
+const merge_entries =  r"""
+          mutation mergeEntries($mrgEntries: [PageEntryUpdInput]!) {
+            mergePageEntries(mrgEntries: $mrgEntries) {
+              code
+              message
+            }
           }
-        }
       """;
 
-const updatepagetitle = r"""
-        mutation updateTitle($pageid: ID!, $title: String!) {
-          updateTitle(pageid: $pageid, title: $title) {
-            code,
-            message
+
+
+/******************************************************************** 
+ name: delete_entries
+ input: list of entry Ids(one or more), format of [{tabpageid, position},{...}]
+        e.g.
+        "delEntries": [
+            {"tabpageid": "21", "position": 33},
+            {"tabpageid": "21", "position": 43}
+          ]
+ output: transaction return code and message. 
+          (return code, 1: succeed, -1: failed)
+**********************************************************************/ 
+const delete_entries =  r"""
+          mutation deleteEntries($delEntries: [EntryIdInput]!) {
+            deletePageEntries(delEntries: $delEntries) {
+              code
+              message
+            }
           }
-        }
-      """;
-
-const deletepagetitle = r"""
-        mutation delTitle($pageid: ID!) {
-          deleteTitle(pageid: $pageid) {
-            code
-            message
-          }
-        }
-      """;
-
-
-const addpageentry = r"""
-      mutation addEntry($pageid: ID!, $position: Int!, $text: String!, $type: EntryType!) {
-        addEntry(pageid: $pageid, position: $position, text: $text, type: $type) {
-              entryid
-              pageid
-              position
-              text
-              type
-          }
-        }
-        """;
-
-const updatepageentry = r"""
-      mutation updateEntry($entryid: ID!, $position: Int!, $text: String!, $type: EntryType!) {
-        updateEntry(entryid: $entryid, position: $position, text: $text, type: $type) {
-          code,
-          message
-        }
-      }
-      """;
-
-const deletepageentry = r"""
-      mutation deleteEntry($entryid: ID!) {
-        deleteEntry(entryid: $entryid) {
-          code
-          message
-        }
-      }
-      """;
-
-const addentrylist =  r"""
-      mutation AddEntryList($newEntryList: [newParamEntry]!) {
-        addEntryList(newEntryList: $newEntryList) {
-          entryid
-        }
-      }
 """;
-/*[newParamEntry] = [{pageid, position, text, type}, ...] */
 
-const updateentrylist =  r"""
-      mutation UpdateEntryList($updEntryList: [ParamEntryInput]!) {
-        updateEntryList(updEntryList: $updEntryList) {
-          code
-          message
-        }
-      }
-""";
-/*[ParamEntryInput] = [{entryid, position, text, type}, ...] */
- 
-const deleteentrylist =  r"""
-      mutation DeleteEntryList($delEntryList: [EntryIdInput]!) {
-        deleteEntryList(delEntryList: $delEntryList) {
-          code,
-          message
-        }
-      }
-""";
-/*[EntryIdInput] = [{entryid}, ...] */
 
--- lib/services/parameter_page/gql_param/queries.dart
+++ lib/services/parameter_page/gql_param/queries.dart
@@ -1,35 +1,52 @@
-const titlequery = r"""
-            query getAllTitles{
-              allTitles {
+/******************************************************* 
+ name: query_allpagetitles
+ input: none
+ output: all the parameter page titles in system
+********************************************************/
+const query_allpagetitles = r"""
+            query getAllPageTitles{
+              allPageTitles {
                 pageid
                 title
                 }
             }
           """;
 
-const pageentryquery = r"""
-            query findtitleentry ($pageid: ID!) {
-              entriesInPageX(pageid: $pageid) {
-                entryid
-                pageid
-                position
-                text
-                type
-              }
-            }
-          """;
 
-const pagequery = r"""
-            query getOnePage($pageid: ID!) {
-              onePage(pageid: $pageid) {
+
+/******************************************************************** 
+ name: query_onepagetree
+ input: pageid (int)
+ output: one complete tree structures and values of one parameter page,
+        including tree title, sub-systems, tabs, sub-pages and entries
+**********************************************************************/
+const query_onepagetree = r"""
+            query getOnePageTree($pageid: ID!) {
+              onePageTree(pageid: $pageid) {
                 pageid
                 title
-                entries {
-                        entryid
+                sub_systems:subsyslist {
+                  subsysid
+                  title
+                  seqnum
+                  tabs:subsystablist {
+                    subsystabid
+                    title
+                    seqnum
+                    sub_pages:tabpagelist {
+                      tabpageid
+                      title
+                      seqnum
+                      entries:pageentrylist {
+                        tabpageid
                         position
                         text
                         type
                       }
+                    }
+                  }
                 }
+              }
             }
           """;
+
