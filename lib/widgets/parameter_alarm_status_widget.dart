7e7f00a6232f7435ee1a701e64c46e627322b640
John Diamond <jdiamond@fnal.gov>
2024-01-16 12:32:25 -0600

    REFACTOR - added padding and a SizedBox for the empty state

--- lib/widgets/parameter_alarm_status_widget.dart
+++ lib/widgets/parameter_alarm_status_widget.dart
@@ -2,22 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_controls_core/service/acsys/service.dart';
 
 class ParameterAlarmStatusWidget extends StatelessWidget {
+  static const iconSize = 16.0;
+
   final AnalogAlarmStatus status;
 
   const ParameterAlarmStatusWidget({super.key, required this.status});
 
   @override
   Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(8.0, 0, 0, 0),
+        child: _buildIcon(context));
+  }
+
+  _buildIcon(BuildContext context) {
     switch (status.state) {
       case AnalogAlarmState.alarming:
         return Icon(Icons.notifications,
-            color: Theme.of(context).colorScheme.error);
+            color: Theme.of(context).colorScheme.error, size: iconSize);
 
       case AnalogAlarmState.bypassed:
-        return const Icon(Icons.notifications_off);
+        return const Icon(Icons.notifications_off, size: iconSize);
 
       case AnalogAlarmState.notAlarming:
-        return Container();
+        return const SizedBox(width: iconSize);
     }
   }
 }
