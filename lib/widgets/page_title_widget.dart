5b24f094403980d5b2c0958e5638e598acbb7bc2
John Diamond <jdiamond@stalag13.net>
2024-02-20 07:08:12 -0600

    REFACTOR - extracted _handleTitleChanged()

--- lib/widgets/page_title_widget.dart
+++ lib/widgets/page_title_widget.dart
@@ -47,10 +47,13 @@ class PageTitleWidget extends StatelessWidget {
         maxLines: 1,
         minLines: 1,
         controller: controller,
-        onEditingComplete: () => onTitleUpdate.call(controller.text),
-        onTapOutside: (PointerDownEvent event) =>
-            onTitleUpdate.call(controller.text),
+        onEditingComplete: () => _handleTitleChanged(),
+        onTapOutside: (_) => _handleTitleChanged(),
       ))
     ]);
+  }
+
+  void _handleTitleChanged() {
+    onTitleUpdate.call(controller.text);
   }
 }
