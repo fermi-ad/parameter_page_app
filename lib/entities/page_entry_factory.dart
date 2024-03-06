beba7586d0e1208c05593b30db0e3e13408d725a
John Diamond <jdiamond@stalag13.net>
2024-03-06 10:49:43 -0600

    REFACTOR - made isACNETDRF and isProcessVariable private since they're only references from PageEntryFactory

--- lib/entities/page_entry_factory.dart
+++ lib/entities/page_entry_factory.dart
@@ -17,30 +17,30 @@ class PageEntryFactory {
     }
   }
 
-  bool isACNETDRF(String val) {
-    var drfRegEx = RegExp(r"^[A-Za-z][:_|][A-Za-z0-9@,]{1,255}$");
-
-    return drfRegEx.hasMatch(val);
-  }
-
-  bool isProcessVariable(String val) {
-    var pvRegEx = RegExp(r"^([A-Za-z0-9:_]{1,255}):([A-Za-z0-9:_]{1,255})$");
-
-    return pvRegEx.hasMatch(val);
-  }
-
   List<PageEntry> _findAllTheParameterEntries({required String inside}) {
     List<String> textArr = inside.split(" ");
     List<PageEntry> ret = [];
 
     for (String textElement in textArr) {
-      if (isACNETDRF(textElement) || isProcessVariable(textElement)) {
+      if (_isACNETDRF(textElement) || _isProcessVariable(textElement)) {
         ret.add(ParameterEntry(textElement,
             label: "", key: Key("parameter_row_$textElement")));
       }
     }
 
     return ret;
+  }
+
+  bool _isACNETDRF(String val) {
+    var drfRegEx = RegExp(r"^[A-Za-z][:_|][A-Za-z0-9@,]{1,255}$");
+
+    return drfRegEx.hasMatch(val);
+  }
+
+  bool _isProcessVariable(String val) {
+    var pvRegEx = RegExp(r"^([A-Za-z0-9:_]{1,255}):([A-Za-z0-9:_]{1,255})$");
+
+    return pvRegEx.hasMatch(val);
   }
 
   bool _isMult(String val) {
