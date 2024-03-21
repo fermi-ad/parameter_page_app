00e56d4259eb4fb2dda1bc71905be05a9a10ff43
John Diamond <jdiamond@stalag13.net>
2024-03-21 14:04:03 -0500

    Remove test that was promoted to the regression test suite

--- test/integration_tests/knob_parameters_in_unison_wip_test.dart
+++ test/integration_tests/knob_parameters_in_unison_wip_test.dart
@@ -1,10 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'helpers/actions.dart';
-import 'helpers/assertions.dart';
-import 'helpers/setup.dart';
-
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
@@ -25,26 +21,6 @@ void main() {
         'Assign knobbing proportion to parameters and save page, proportions are persisted',
         (WidgetTester tester) async {
       // This test might belong with Knob Parameter
-    });
-
-    testWidgets(
-        'Create an empty mult, empty mult entry is shown with description',
-        (WidgetTester tester) async {
-      // Given a blank parameter page in edit mode
-      await startParameterPageApp(tester);
-      await createNewParameterPage(tester);
-
-      // When I add a mult with N = 0 and a description of 'test mult' (mult:0 test mult)
-      await addANewEntry(tester, "mult:0 test mult");
-
-      // ... and I exit edit mode
-      await exitEditMode(tester);
-
-      // Then the empty mult is displayed
-      assertMult(isInRow: 0, hasN: 0, hasDescription: "test mult");
-
-      // ... and it contains no parameters
-      assertMultContains(atIndex: 1, parameters: []);
     });
 
     testWidgets(
