238d1079212cfc646f0e5ef383193bb1b16168d6
John Diamond <jdiamond@stalag13.net>
2024-03-22 09:52:04 -0500

    REFACTOR - converted MultEntryWidget to a StatefulWidget

--- lib/widgets/mult_entry_widget.dart
+++ lib/widgets/mult_entry_widget.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:parameter_page/widgets/page_entry_widget.dart';
 
-class MultEntryWidget extends StatelessWidget {
+class MultEntryWidget extends StatefulWidget {
   final int numberOfEntries;
+
   final String description;
 
   const MultEntryWidget(
       {super.key, required this.numberOfEntries, this.description = ""});
 
+  @override
+  State<StatefulWidget> createState() {
+    return _MultEntryWidgetState();
+  }
+}
+
+class _MultEntryWidgetState extends State<MultEntryWidget> {
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
@@ -16,7 +24,7 @@ class MultEntryWidget extends StatelessWidget {
             shape: RoundedRectangleBorder(
                 side: BorderSide(color: colorScheme.surface, width: 2.0),
                 borderRadius: BorderRadius.circular(4.0)),
-            child: Text("mult:$numberOfEntries $description",
+            child: Text("mult:${widget.numberOfEntries} ${widget.description}",
                 style: TextStyle(color: colorScheme.secondary))));
   }
 }
