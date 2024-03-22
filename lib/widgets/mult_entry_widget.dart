548bd0524894394d82e79ff3758d89a0abcda405
John Diamond <jdiamond@stalag13.net>
2024-03-22 12:20:27 -0500

    REFACTOR - extracted _buildDisplayMode()

--- lib/widgets/mult_entry_widget.dart
+++ lib/widgets/mult_entry_widget.dart
@@ -19,28 +19,32 @@ class _MultEntryWidgetState extends State<MultEntryWidget> {
   @override
   Widget build(BuildContext context) {
     return PageEntryWidget(
-        child: InkWell(
-            onTap: () => setState(() => _active = !_active),
-            child: Card(
-                shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                        color: _active
-                            ? _colorScheme.secondaryContainer
-                            : _colorScheme.surface,
-                        width: 2.0),
-                    borderRadius: BorderRadius.circular(4.0)),
-                child: ConstrainedBox(
-                    constraints: const BoxConstraints(minHeight: 34.0),
-                    child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 40.0),
-                        child: _buildEntryText())))));
+        child: InkWell(onTap: _handleTap, child: _buildDisplayMode()));
+  }
+
+  Widget _buildDisplayMode() {
+    return Card(
+        shape: RoundedRectangleBorder(
+            side: BorderSide(
+                color: _active
+                    ? _colorScheme.secondaryContainer
+                    : _colorScheme.surface,
+                width: 2.0),
+            borderRadius: BorderRadius.circular(4.0)),
+        child: ConstrainedBox(
+            constraints: const BoxConstraints(minHeight: 34.0),
+            child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 40.0),
+                child: _buildEntryText())));
   }
 
   Widget _buildEntryText() {
     return Text("mult:${widget.numberOfEntries} ${widget.description}",
         style: TextStyle(color: _colorScheme.secondary));
   }
+
+  void _handleTap() => setState(() => _active = !_active);
 
   ColorScheme get _colorScheme => Theme.of(context).colorScheme;
 
