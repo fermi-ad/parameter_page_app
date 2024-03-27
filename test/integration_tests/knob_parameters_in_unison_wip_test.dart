27da31e9489fc501544bb06ae7bd71cad20b5256
John Diamond <jdiamond@stalag13.net>
2024-03-27 15:47:59 -0500

    RED - failing acceptance test for removing focus when the user taps outside of the enabled mult

--- test/integration_tests/knob_parameters_in_unison_wip_test.dart
+++ test/integration_tests/knob_parameters_in_unison_wip_test.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:parameter_page/services/settings_permission/settings_permission_service.dart';
 
 import 'helpers/actions.dart';
 import 'helpers/assertions.dart';
@@ -27,6 +28,28 @@ void main() {
         'Assign knobbing proportion to parameters and save page, proportions are persisted',
         (WidgetTester tester) async {
       // This test might belong with Knob Parameter
+    });
+
+    testWidgets('Tap outside of enabled mult, disable mult',
+        (WidgetTester tester) async {
+      // Given a new parameter page with a mult entry
+      await startParameterPageApp(tester);
+      await createNewParameterPage(tester);
+      await addANewEntry(tester, "mult:0 test mult #1");
+      await exitEditMode(tester);
+
+      // ... and settings have been enabled
+      await requestSettingsPermission(tester,
+          forDuration: SettingsRequestDuration.indefinitely);
+
+      // ... and test mult #1 is already enabled
+      await tapPageEntry(tester, atRowIndex: 0);
+
+      // When I tap outside of the mult
+      await tester.tap(find.text("New Parameter Page"));
+
+      // Then the mult is in the disabled state
+      assertMultState(tester, atIndex: 0, isEnabled: false);
     });
 
     testWidgets(
