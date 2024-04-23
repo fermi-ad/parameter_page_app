cb6d4218749ca1d3a893eb2f1d4de7fbe4705741
John Diamond <jdiamond@stalag13.net>
2024-04-23 15:21:55 -0500

    Promote acceptance test to the regression test suite

--- test/integration_tests/login_test.dart
+++ test/integration_tests/login_test.dart
@@ -19,5 +19,19 @@ void main() {
       // Then the user's username should be displayed
       expect(find.text("Logged in as testuser"), findsOneWidget);
     });
+
+    testWidgets('Tap Logout, user is logged out', (WidgetTester tester) async {
+      // Given the application has been launched and the user is already logged in
+      await startParameterPageApp(tester);
+
+      // When I open the main menu and tap "Logout"
+      await openMainMenu(tester);
+      await tester.tap(find.text("Logout"));
+      await tester.pumpAndSettle();
+
+      // Then the username changes to "not logged in"
+      expect(find.text("Logged in as testuser"), findsNothing);
+      expect(find.text("You are not logged in"), findsOneWidget);
+    });
   });
 }
--- test/integration_tests/login_wip_test.dart
+++ test/integration_tests/login_wip_test.dart
@@ -1,25 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'helpers/actions.dart';
-import 'helpers/setup.dart';
-
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('Login tests (work in progress)', () {
-    testWidgets('Tap Logout, user is logged out', (WidgetTester tester) async {
-      // Given the application has been launched and the user is already logged in
-      await startParameterPageApp(tester);
-
-      // When I open the main menu and tap "Logout"
-      await openMainMenu(tester);
-      await tester.tap(find.text("Logout"));
-      await tester.pumpAndSettle();
-
-      // Then the username changes to "not logged in"
-      expect(find.text("Logged in as testuser"), findsNothing);
-      expect(find.text("You are not logged in"), findsOneWidget);
-    });
-  });
+  group('Login tests (work in progress)', () {});
 }
