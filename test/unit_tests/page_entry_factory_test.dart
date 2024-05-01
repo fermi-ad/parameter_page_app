98dd1cb651d78b70934ed807c2ab7cde06b7a2d0
John Diamond <jdiamond@fnal.gov>
2024-05-01 07:30:08 -0500

    REFACTOR - extracted _createEntryAndAssertProportion(..)

--- test/unit_tests/page_entry_factory_test.dart
+++ test/unit_tests/page_entry_factory_test.dart
@@ -20,60 +20,51 @@ void main() {
         "Input contains no *, produces a ParameterEntry with a proportion of 1",
         () {
       // Given a PageEntryFactory
-      final factory = PageEntryFactory();
-
       // When I createEntries(fromInput:) with a proportion
-      final newEntry = factory.createEntries(fromInput: "G:MULT1")[0];
-
       // Then the ParameterEntry has a proportion of 0.5
-      expect(newEntry.typeAsString, "Parameter");
-      expect(newEntry.entryText(), "G:MULT1");
-      expect(newEntry.proportion, 1);
+      _createEntryAndAssertProportion(
+          fromInput: "G:MULT1", parameterIs: "G:MULT1", proportionIs: 1);
     });
 
     test(
         "Input contains * followed by a number, produces a ParameterEntry with a proportion",
         () {
       // Given a PageEntryFactory
-      final factory = PageEntryFactory();
-
       // When I createEntries(fromInput:) with a proportion
-      final newEntry = factory.createEntries(fromInput: "G:MULT1*0.5")[0];
-
       // Then the ParameterEntry has a proportion of 0.5
-      expect(newEntry.typeAsString, "Parameter");
-      expect(newEntry.entryText(), "G:MULT1");
-      expect(newEntry.proportion, 0.5);
+      _createEntryAndAssertProportion(
+          fromInput: "G:MULT1*0.5", parameterIs: "G:MULT1", proportionIs: 0.5);
     });
 
     test(
         "Input contains * followed by a negative number, produces a ParameterEntry with a negative proportion",
         () {
       // Given a PageEntryFactory
-      final factory = PageEntryFactory();
-
       // When I createEntries(fromInput:) with a proportion
-      final newEntry = factory.createEntries(fromInput: "G:MULT1*-1")[0];
-
       // Then the ParameterEntry has a proportion of -1
-      expect(newEntry.typeAsString, "Parameter");
-      expect(newEntry.entryText(), "G:MULT1");
-      expect(newEntry.proportion, -1.0);
+      _createEntryAndAssertProportion(
+          fromInput: "G:MULT1*-1", parameterIs: "G:MULT1", proportionIs: -1.0);
     });
 
     test(
         "Input contains * followed by a positive number with a +, produces a ParameterEntry with a positive proportion",
         () {
       // Given a PageEntryFactory
-      final factory = PageEntryFactory();
-
       // When I createEntries(fromInput:) with a proportion
-      final newEntry = factory.createEntries(fromInput: "G:MULT1*+2.5")[0];
-
       // Then the ParameterEntry has a proportion of -1
-      expect(newEntry.typeAsString, "Parameter");
-      expect(newEntry.entryText(), "G:MULT1");
-      expect(newEntry.proportion, 2.5);
+      _createEntryAndAssertProportion(
+          fromInput: "G:MULT1*+2.5", parameterIs: "G:MULT1", proportionIs: 2.5);
     });
   });
+}
+
+void _createEntryAndAssertProportion(
+    {required String fromInput,
+    required String parameterIs,
+    required double proportionIs}) {
+  final factory = PageEntryFactory();
+  final newEntry = factory.createEntries(fromInput: fromInput)[0];
+  expect(newEntry.typeAsString, "Parameter");
+  expect(newEntry.entryText(), parameterIs);
+  expect(newEntry.proportion, proportionIs);
 }
