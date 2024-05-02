27db25dddd8aaedccf530e48b9cd4913140babe7
John Diamond <jdiamond@fnal.gov>
2024-05-02 14:20:27 -0500

    Promoted acceptance test to the regression test suite

--- test/integration_tests/knob_parameter_test.dart
+++ test/integration_tests/knob_parameter_test.dart
@@ -171,5 +171,58 @@ void main() {
       // Then the proportion is displayed with the parameter
       assertParameterHasDetails("G:MULT1", proportion: "0.555");
     });
+
+    testWidgets(
+        'Tap setting control for parameter with proportion assigned, step size display includes the proportion (step size * proportion)',
+        (WidgetTester tester) async {
+      // Given a new page
+      await startParameterPageApp(tester);
+      await createNewParameterPage(tester);
+
+      // ... and a parameter with a knobbing proportion of 200
+      await addANewEntry(tester, "G:MULT1*200");
+      await exitEditMode(tester);
+      await waitForDataToLoadFor(tester, "G:MULT1");
+
+      // ... and settings are enabled
+      await requestSettingsPermission(tester,
+          forDuration: SettingsRequestDuration.indefinitely);
+
+      // When I tap the setting value
+      await tapSetting(tester, forDRF: "G:MULT1");
+
+      // Then the step size is scaled according to the proportion (step size * proportion)
+      assertKnobbing(stepSizeIs: "1.0", forDRF: "G:MULT1");
+    });
+
+    testWidgets(
+        'Knob parameter with proportion up multiple steps, proportion applied to each step',
+        (WidgetTester tester) async {
+      // Given a new page
+      await startParameterPageApp(tester);
+      await createNewParameterPage(tester);
+
+      // ... and a parameter with a knobbing proportion of 200
+      await addANewEntry(tester, "G:MULT1*200");
+      await exitEditMode(tester);
+      await waitForDataToLoadFor(tester, "G:MULT1");
+
+      // ... and settings are enabled
+      await requestSettingsPermission(tester,
+          forDuration: SettingsRequestDuration.indefinitely);
+
+      // When I tap the setting value
+      await tapSetting(tester, forDRF: "G:MULT1");
+
+      // ... and knob up ten times
+      for (int i = 0; i != 10; i++) {
+        await knobUp(tester, steps: 1);
+
+        // Then the text field value is adjusted on each step
+        final expected = 50.0 + i + 1;
+        assertSettingTextInputValue(
+            forDRF: "G:MULT1", isSetTo: expected.toStringAsPrecision(4));
+      }
+    });
   });
 }
--- test/integration_tests/knob_parameters_in_unison_wip_test.dart
+++ test/integration_tests/knob_parameters_in_unison_wip_test.dart
@@ -13,59 +13,6 @@ void main() {
 
   group('Knob Parameters in Unison (Mults)', () {
     testWidgets(
-        'Tap setting control for parameter with proportion assigned, step size display includes the proportion (step size * proportion)',
-        (WidgetTester tester) async {
-      // Given a new page
-      await startParameterPageApp(tester);
-      await createNewParameterPage(tester);
-
-      // ... and a parameter with a knobbing proportion of 200
-      await addANewEntry(tester, "G:MULT1*200");
-      await exitEditMode(tester);
-      await waitForDataToLoadFor(tester, "G:MULT1");
-
-      // ... and settings are enabled
-      await requestSettingsPermission(tester,
-          forDuration: SettingsRequestDuration.indefinitely);
-
-      // When I tap the setting value
-      await tapSetting(tester, forDRF: "G:MULT1");
-
-      // Then the step size is scaled according to the proportion (step size * proportion)
-      assertKnobbing(stepSizeIs: "1.0", forDRF: "G:MULT1");
-    });
-
-    testWidgets(
-        'Knob parameter with proportion up multiple steps, proportion applied to each step',
-        (WidgetTester tester) async {
-      // Given a new page
-      await startParameterPageApp(tester);
-      await createNewParameterPage(tester);
-
-      // ... and a parameter with a knobbing proportion of 200
-      await addANewEntry(tester, "G:MULT1*200");
-      await exitEditMode(tester);
-      await waitForDataToLoadFor(tester, "G:MULT1");
-
-      // ... and settings are enabled
-      await requestSettingsPermission(tester,
-          forDuration: SettingsRequestDuration.indefinitely);
-
-      // When I tap the setting value
-      await tapSetting(tester, forDRF: "G:MULT1");
-
-      // ... and knob up ten times
-      for (int i = 0; i != 10; i++) {
-        await knobUp(tester, steps: 1);
-
-        // Then the text field value is adjusted on each step
-        final expected = 50.0 + i + 1;
-        assertSettingTextInputValue(
-            forDRF: "G:MULT1", isSetTo: expected.toStringAsPrecision(4));
-      }
-    });
-
-    testWidgets(
         'Assign knobbing proportion to parameters and save page, proportions are persisted',
         (WidgetTester tester) async {
       // This test might belong with Knob Parameter
