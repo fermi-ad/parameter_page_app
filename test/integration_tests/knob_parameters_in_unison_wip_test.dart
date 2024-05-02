3074eeda36143ed37109ecd72e170cb863a5f873
John Diamond <jdiamond@fnal.gov>
2024-05-02 10:08:28 -0500

    RED - failing acceptance test for knobbing a parameter with a proportion set

--- test/integration_tests/knob_parameters_in_unison_wip_test.dart
+++ test/integration_tests/knob_parameters_in_unison_wip_test.dart
@@ -15,7 +15,31 @@ void main() {
     testWidgets(
         'Knob parameter with proportion up multiple steps, proportion applied to each step',
         (WidgetTester tester) async {
-      // This test might belong with Knob Parameter
+      // Given a new page
+      await startParameterPageApp(tester);
+      await createNewParameterPage(tester);
+
+      // ... and a parameter with a knobbing proportion of 200
+      await addANewEntry(tester, "G:MULT1*200");
+      await exitEditMode(tester);
+      await waitForDataToLoadFor(tester, "G:MULT1");
+
+      // ... and settings are enabled
+      await requestSettingsPermission(tester,
+          forDuration: SettingsRequestDuration.indefinitely);
+
+      // When I tap the setting value
+      await tapSetting(tester, forDRF: "G:MULT1");
+
+      // ... and knob up ten times
+      for (int i = 0; i != 10; i++) {
+        await knobUp(tester, steps: 1);
+
+        // Then the text field value is adjusted on each step
+        final expected = 50.0 + i + 1;
+        assertSettingTextInputValue(
+            forDRF: "G:MULT1", isSetTo: expected.toStringAsPrecision(4));
+      }
     });
 
     testWidgets(
