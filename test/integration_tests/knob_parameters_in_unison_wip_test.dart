2e96d8f948f1e4705215d5d4b38618a255032a69
John Diamond <jdiamond@fnal.gov>
2024-05-14 09:56:57 -0500

    Promoted test to the regression test suite

--- test/integration_tests/knob_parameters_in_unison_wip_test.dart
+++ test/integration_tests/knob_parameters_in_unison_wip_test.dart
@@ -17,32 +17,6 @@ void main() {
         (WidgetTester tester) async {},
         semanticsEnabled: false);
 
-    testWidgets('Save new page containing mults, mults are persisted',
-        (WidgetTester tester) async {
-      // Given a new parameter page titled "New Page With Mult" with a mult containing three parameters
-      await startParameterPageApp(tester);
-      await createNewParameterPage(tester);
-      await changePageTitle(tester, to: "New Page With Mult");
-      await addANewEntry(tester, "mult:3 test mult");
-      await addANewEntry(tester, "G:MULT1");
-      await addANewEntry(tester, "G:MULT2");
-      await addANewEntry(tester, "G:MULT3");
-      await exitEditMode(tester);
-
-      // When I save the new page, start a new page and then open the saved page again
-      await openMainMenu(tester);
-      await saveParameterPage(tester);
-      await waitForPageToBeSaved(tester);
-      await newPage(tester);
-      await navigateToOpenPage(tester);
-      await openParameterPage(tester, withTitle: "New Page With Mult");
-
-      // Then the page contains the mults and the three parameters that belong to it
-      assertMult(isInRow: 0, hasN: 3, hasDescription: "test mult");
-      assertMultContains(
-          atIndex: 0, parameters: ["G:MULT1", "G:MULT2", "G:MULT3"]);
-    }, semanticsEnabled: false);
-
     testWidgets('Add mults to existing page and save, mults are persisted',
         (WidgetTester tester) async {},
         semanticsEnabled: false);
