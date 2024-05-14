470a93dc139f355aa9c8f6877a7e765d1508b3b6
John Diamond <jdiamond@fnal.gov>
2024-05-14 15:14:25 -0500

    Move test to the regression test suite

--- test/integration_tests/knob_parameters_in_unison_wip_test.dart
+++ test/integration_tests/knob_parameters_in_unison_wip_test.dart
@@ -16,33 +16,5 @@ void main() {
         'Knob a single parameter inside of a mult, only that parameter changes',
         (WidgetTester tester) async {},
         semanticsEnabled: false);
-
-    testWidgets('Add mults to existing page and save, mults are persisted',
-        (WidgetTester tester) async {
-      // Given "Test Page 2" has been loaded
-      await startParameterPageApp(tester);
-      await navigateToOpenPage(tester);
-      await openParameterPage(tester, withTitle: "Test Page 2");
-
-      // ... and a mult containing three parameters has been added
-      await enterEditMode(tester);
-      await addANewEntry(tester, "mult:3 test mult");
-      await addANewEntry(tester, "G:MULT1");
-      await addANewEntry(tester, "G:MULT2");
-      await addANewEntry(tester, "G:MULT3");
-      await exitEditMode(tester);
-
-      // When I save the new page, start a new page and then open the saved page again
-      await openMainMenu(tester);
-      await saveParameterPage(tester);
-      await waitForPageToBeSaved(tester);
-      await newPage(tester);
-      await navigateToOpenPage(tester);
-      await openParameterPage(tester, withTitle: "Test Page 2");
-
-      // Then the mult and the entries that belong to it have persisted
-      assertMult(hasN: 3, hasDescription: "test mult");
-      assertMultContains(parameters: ["G:MULT1", "G:MULT2", "G:MULT3"]);
-    }, semanticsEnabled: false);
   });
 }
