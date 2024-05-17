e1c374bd422586908811cf4e5ee6b83d630b1d33
John Diamond <jdiamond@fnal.gov>
2024-05-17 11:36:34 -0500

    Promote test to the regression test suite

--- test/integration_tests/knob_parameters_in_unison_test.dart
+++ test/integration_tests/knob_parameters_in_unison_test.dart
@@ -401,4 +401,36 @@ void main() {
     assertMult(hasN: 3, hasDescription: "test mult");
     assertMultContains(parameters: ["G:MULT1", "G:MULT2", "G:MULT3"]);
   }, semanticsEnabled: false);
+
+  testWidgets(
+      'Knob a single parameter inside of a mult, only that parameter changes',
+      (WidgetTester tester) async {
+    // Given a new parameter page
+    await startParameterPageApp(tester);
+    await createNewParameterPage(tester);
+
+    // ... and I have create a mult:3 with three parameters
+    await addANewEntry(tester, "mult:3 test mult");
+    await addANewEntry(tester, "G:MULT1");
+    await addANewEntry(tester, "G:MULT2");
+    await addANewEntry(tester, "G:AMANDA");
+    await exitEditMode(tester);
+
+    // ... and data for G:AMANDA has been loaded
+    await waitForDataToLoadFor(tester, "G:AMANDA");
+
+    // ... and settings are enabled
+    await requestSettingsPermission(tester,
+        forDuration: SettingsRequestDuration.indefinitely);
+
+    // When I tap the setting property
+    await tapSetting(tester, forDRF: "G:AMANDA");
+    assertSettingTextInputValue(forDRF: "G:AMANDA", isSetTo: "50.00");
+
+    // ... and then knob up by 1 step
+    await knobUp(tester, steps: 1);
+
+    // Then the value displayed in the setting control's text field is...
+    assertSettingTextInputValue(forDRF: "G:AMANDA", isSetTo: "51.00");
+  }, semanticsEnabled: false);
 }
--- test/integration_tests/knob_parameters_in_unison_wip_test.dart
+++ test/integration_tests/knob_parameters_in_unison_wip_test.dart
@@ -11,37 +11,5 @@ import 'helpers/setup.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('Knob Parameters in Unison (Mults)', () {
-    testWidgets(
-        'Knob a single parameter inside of a mult, only that parameter changes',
-        (WidgetTester tester) async {
-      // Given a new parameter page
-      await startParameterPageApp(tester);
-      await createNewParameterPage(tester);
-
-      // ... and I have create a mult:3 with three parameters
-      await addANewEntry(tester, "mult:3 test mult");
-      await addANewEntry(tester, "G:MULT1");
-      await addANewEntry(tester, "G:MULT2");
-      await addANewEntry(tester, "G:AMANDA");
-      await exitEditMode(tester);
-
-      // ... and data for G:AMANDA has been loaded
-      await waitForDataToLoadFor(tester, "G:AMANDA");
-
-      // ... and settings are enabled
-      await requestSettingsPermission(tester,
-          forDuration: SettingsRequestDuration.indefinitely);
-
-      // When I tap the setting property
-      await tapSetting(tester, forDRF: "G:AMANDA");
-      assertSettingTextInputValue(forDRF: "G:AMANDA", isSetTo: "50.00");
-
-      // ... and then knob up by 1 step
-      await knobUp(tester, steps: 1);
-
-      // Then the value displayed in the setting control's text field is...
-      assertSettingTextInputValue(forDRF: "G:AMANDA", isSetTo: "51.00");
-    }, semanticsEnabled: false);
-  });
+  group('Knob Parameters in Unison (Mults)', () {});
 }
